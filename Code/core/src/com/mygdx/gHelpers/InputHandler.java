9b60cb484694a0d74fefdaabbd4d94868e08d12f
Linkevich <32456897+Linkevich@users.noreply.github.com>
2017-12-08 00:11:33 +0300

    Add new classes

--- Code/core/src/com/mygdx/gHelpers/InputHandler.java
+++ Code/core/src/com/mygdx/gHelpers/InputHandler.java
@@ -1,5 +1,135 @@
 package com.mygdx.gHelpers;
 
-public class InputHandler {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+
+import com.mygdx.gameobjects.Bird;
+import com.mygdx.gameworld.GameWorld;
+import com.mygdx.ui.SimpleButton;
+
+public class InputHandler implements InputProcessor {
+    private Bird myBird;
+    private GameWorld myWorld;
+
+    private List<SimpleButton> menuButtons;
+
+    private SimpleButton playButton;
+
+    private float scaleFactorX;
+    private float scaleFactorY;
+
+    public InputHandler(GameWorld myWorld, float scaleFactorX,
+            float scaleFactorY) {
+        this.myWorld = myWorld;
+        myBird = myWorld.getBird();
+
+        int midPointY = myWorld.getMidPointY();
+
+        this.scaleFactorX = scaleFactorX;
+        this.scaleFactorY = scaleFactorY;
+
+        menuButtons = new ArrayList<SimpleButton>();
+        playButton = new SimpleButton(
+                136 / 2 - (AssetLoader.playButtonUp.getRegionWidth() / 2),
+                midPointY + 50, 29, 16, AssetLoader.playButtonUp,
+                AssetLoader.playButtonDown);
+        menuButtons.add(playButton);
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        screenX = scaleX(screenX);
+        screenY = scaleY(screenY);
+        System.out.println(screenX + " " + screenY);
+        if (myWorld.isMenu()) {
+            playButton.isTouchDown(screenX, screenY);
+        } else if (myWorld.isReady()) {
+            myWorld.start();
+        }
+
+        myBird.onClick();
+
+        if (myWorld.isGameOver() || myWorld.isHighScore()) {
+            myWorld.restart();
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        screenX = scaleX(screenX);
+        screenY = scaleY(screenY);
+
+        if (myWorld.isMenu()) {
+            if (playButton.isTouchUp(screenX, screenY)) {
+                myWorld.ready();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+
+        if (keycode == Keys.SPACE) {
+
+            if (myWorld.isMenu()) {
+                myWorld.ready();
+            } else if (myWorld.isReady()) {
+                myWorld.start();
+            }
+
+            myBird.onClick();
+
+            if (myWorld.isGameOver() || myWorld.isHighScore()) {
+                myWorld.restart();
+            }
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+    private int scaleX(int screenX) {
+        return (int) (screenX / scaleFactorX);
+    }
+
+    private int scaleY(int screenY) {
+        return (int) (screenY / scaleFactorY);
+    }
+
+    public List<SimpleButton> getMenuButtons() {
+        return menuButtons;
+    }
 }
--- Code/core/src/com/mygdx/game/MyGdxGame.java
+++ Code/core/src/com/mygdx/game/MyGdxGame.java
@@ -7,13 +7,21 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import com.mygdx.screens.GameScreen;
+import com.mygdx.gHelpers.AssetLoader;
+import com.mygdx.screens.SplashScreen;
 
 public class MyGdxGame extends Game {
 	
 	
-	@Override
-	public void create(){
-		Gdx.app.log("MyGame", "created");
-		setScreen(new GameScreen());
-	}
+	 @Override
+	    public void create() {
+	        AssetLoader.load();
+	        setScreen(new SplashScreen(this));
+	    }
+
+	    @Override
+	    public void dispose() {
+	        super.dispose();
+	        AssetLoader.dispose();
+	    }
 }
